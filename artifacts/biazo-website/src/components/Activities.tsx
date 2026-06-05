f9a0351c3c564e9799a4b84878180187d73fc48b
Mickoscofiled <mikiasdereje45@gmail.com>
2026-06-05 14:14:48 +0300

    Revert "Add Cables sector to Key Activities"
    This reverts commit a2f61ad9e323c84da2df3b8363c4c2fa0f1cd415.

--- artifacts/biazo-website/src/components/Activities.tsx
+++ artifacts/biazo-website/src/components/Activities.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Tractor, HardHat, Pickaxe, Flame, FlaskConical, Wrench, PipetteIcon, AirVent, Monitor, Cable } from "lucide-react";
+import { Tractor, HardHat, Pickaxe, Flame, FlaskConical, Wrench, PipetteIcon, AirVent, Monitor } from "lucide-react";
 
 import agriImg from "@/assets/sectors/agriculture.png";
 import constImg from "@/assets/sectors/construction.png";
@@ -10,7 +10,6 @@ import weldingImg from "@/assets/sectors/cutting-welding.jpg";
 import pipesImg from "@/assets/sectors/pipes-fittings.png";
 import acImg from "@/assets/sectors/air-conditioners.png";
 import itImg from "@/assets/sectors/it-networking.png";
-import cablesImg from "@/assets/sectors/cables.jpg";
 
 const sectors = [
   { name: "Agriculture Equipment", icon: <Tractor className="w-5 h-5" /> },
@@ -21,8 +20,7 @@ const sectors = [
   { name: "Cutting and welding equipment and consumables", icon: <Wrench className="w-5 h-5" /> },
   { name: "Pipes and fittings, valves and couplings", icon: <PipetteIcon className="w-5 h-5" /> },
   { name: "Air conditioners, Accessories and spare parts", icon: <AirVent className="w-5 h-5" /> },
-  { name: "IT Equipment, Networking & Communication", icon: <Monitor className="w-5 h-5" /> },
-  { name: "Trailing Cable – Flexible Mining Cables, UTP & STP Cables", icon: <Cable className="w-5 h-5" /> }
+  { name: "IT Equipment, Networking & Communication", icon: <Monitor className="w-5 h-5" /> }
 ];
 
 const sectorImages = [
@@ -35,7 +33,6 @@ const sectorImages = [
   { name: "Pipes and fittings, valves and couplings", image: pipesImg },
   { name: "Air conditioners, Accessories and spare parts", image: acImg },
   { name: "IT Equipment, Networking & Communication", image: itImg },
-  { name: "Trailing Cable – Flexible Mining Cables, UTP & STP Cables", image: cablesImg },
 ];
 
 export default function Activities() {
